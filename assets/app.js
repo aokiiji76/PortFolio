e28066b0bb740aba77f92bbad48096fa679ef523
aokiiji76 <aokiiji76@gmail.com>
2021-06-28 17:36:41 +0200

    app.js

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,24 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+//import './styles/app.css'
+import './scss/portfolio.scss'
+//import './scss/_variables.scss'
 
 // start the Stimulus application
-import './bootstrap';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/scss/bootstrap.scss'
+import 'jquery';
+
+
+// import './images/001-target.svg';
+// import './images/002-development.svg';
+// import './images/003-idea.svg';
+// import './images/004-smartphone.svg';
+// import './images/download.svg';
+// import './images/hamburger.svg';
+// import './images/puzzle.svg';
+
+
+import './js/live-resume.js';
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,13 +10,11 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
-     *
      */
     public function home(): Response
     {
         return $this->render('home/home.html.twig', [
-
-
+            'controller_name' => 'HomeController',
         ]);
     }
 }
