a752dc223b982d6a44656308627b8effb97f47f6
aokiiji76 <aokiiji76@gmail.com>
2021-06-30 15:12:40 +0200

    home ok

--- assets/app.js
+++ assets/app.js
@@ -1,25 +1,32 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
 
-// any CSS you import will output into a single css file (app.css in this case)
-
+//scss
 import './styles/app.scss';
-//import "../vendors/bootstrap/scss/bootstrap.scss";
-// start the Stimulus application
-import './bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
+//css
+import './styles/css/portfolio.css';
 
-import 'jquery/dist/jquery.min';
-import '@popperjs/core/dist/umd/popper-base.min';
-import 'bootstrap';
+//js
 import './js/portfolio.js';
 
-import '@fortawesome/fontawesome-free/css/all.min.css'
 
-// import '@fortawesome/fontawesome-free/js/fontawesome'
-// import '@fortawesome/fontawesome-free/js/solid'
-// import '@fortawesome/fontawesome-free/js/regular'
-// import '@fortawesome/fontawesome-free/js/brands'
+//js bootstrap
+import 'bootstrap';
+//font
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+
+
+
+
+
+//import './bootstrap';
+
+//import "@bootstrap/scss";
+//import 'jquery/dist/jquery.min';
+//import '@popperjs/core/dist/umd/popper-base.min';
+//import 'bootstrap';
+
+
+
+
