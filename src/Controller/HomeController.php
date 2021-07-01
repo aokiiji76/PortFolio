a7fb133206430d33ce2c505ceab50fb34970fd3e
aokiiji76 <aokiiji76@gmail.com>
2021-07-01 18:32:21 +0200

    download

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -14,7 +17,19 @@ class HomeController extends AbstractController
     public function home(): Response
     {
         return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
+
         ]);
     }
+
+    /**
+     * @Route("/download", name="download_file")
+     **/
+    public function downloadFileAction(): BinaryFileResponse
+    {
+        $file = '../assets/pdf/cv.pdf';
+        $response = new BinaryFileResponse($file);
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT,'cv.pdf');
+        return $response;
+    }
+
 }
