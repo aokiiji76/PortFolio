dfe49a1212acae9deaf1927cd624570d50e0c2cb
aokiiji76 <aokiiji76@gmail.com>
2021-07-14 16:06:25 +0200

    clean

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,3 @@
-
-//scss
-//import './styles/app.scss';
-//import 'bootstrap/scss/bootstrap.scss';
 //css
 import './styles/css/portfolio.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,25 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './js/entry';
 import './js/portfolio.js';
 
-
 //js bootstrap
 import 'bootstrap';
 
 //font
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-
-
-
-
-
-
-//import './bootstrap';
-
-//import "@bootstrap/scss";
-//import 'jquery/dist/jquery.min';
+//popperjs
 import '@popperjs/core/dist/umd/popper-base.min';
-//import 'bootstrap';
+
 
 
 
